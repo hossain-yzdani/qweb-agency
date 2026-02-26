318df4f3e5802b846432fe1d3ed9aa568738a9b3
HossainYazdani <hossain.yazdaniii@gmail.com>
2026-02-26 14:30:02 +0330

    fix bug reverse navbar in mobile

--- components/basic/navabr.tsx
+++ components/basic/navabr.tsx
@@ -30,68 +30,60 @@ const Header = () => {
 
 
   return (
-  <>
-  <nav className="fixed px-2 py-1 md:py-2 top-2 left-2 right-2 z-50 glass rounded-xl">
+    <>
+      <nav className="fixed px-2 py-1 md:py-2 top-2 left-2 right-2 z-50 glass rounded-xl">
+        <div className="flex justify-between items-center flex-row-reverse md:flex-row">
+          {/* logo & title */}
+          <div className="flex items-center flex-1 z-15">
+            <Image
+              src="/logo/qweb-logo.png"
+              alt="Picture of the author"
+              width={50}
+              height={50}
+              className="w-13 md:w-15 rounded-lg"
+            />
+          </div>
 
-      <div className="flex justify-between items-center">
+          {/* routes */}
+          <div className="flex-2 hidden md:block z-15">
+            <ul className="flex items-center justify-center">
+              {links.map((e) => (
+                <li key={e.url} className="flex p-2">
+                  <Link
+                    className={`nav-link text-linkcolor ${e.url === pathname ? "text-white" : ""}`}
+                    href={e.url}
+                  >
+                    {e.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        {/* logo & title */}
-        <div
-          className="flex flex-row-reverse md:flex-row items-center flex-1 z-15">
-          <Image
-            src="/logo/qweb-logo.png"
-            alt="Picture of the author"
-            width={50}
-            height={50}
-            className="w-13 md:w-15 rounded-lg"
-          />
-        </div>
-
-        {/* routes */}
-        <div
-          className="flex-2 hidden md:block z-15">
-          <ul className="flex items-center justify-center">
-            {links.map((e) => (
-              <li key={e.url} className="flex p-2">
-                <Link
-                  className={`nav-link text-linkcolor ${e.url === pathname ? "text-white" : ""}`}
-                  href={e.url}
-                >
-                  {e.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        {/* login */}
-        <div
-          dir="ltr"
-          className="flex-1 pl-4 text-white hidden md:flex flex-row z-15"
-        >
-          <Link
-            className="w-1/2 flex justify-center items-center px-4 py-2 rounded-full bg-themecolor btn-glow"
-            href={""}
+          {/* login */}
+          <div
+            dir="ltr"
+            className="flex-1 pl-4 text-white hidden md:flex flex-row z-15"
           >
-            ثبت نام
-          </Link>
+            <Link
+              className="w-1/2 flex justify-center items-center px-4 py-2 rounded-full bg-themecolor btn-glow"
+              href={""}
+            >
+              ثبت نام
+            </Link>
+          </div>
+
+          {/* mobile button */}
+          <div className="block md:hidden  z-15">
+            <button
+              className=" justify-center pl-1 text-white items-center rounded cursor-pointer text-2xl"
+              type="button"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              ☰
+            </button>
+          </div>
         </div>
-
-        {/* mobile button */}
-        <div
-          className="block md:hidden  z-15">
-          <button
-            className=" justify-center pl-1 text-white items-center rounded cursor-pointer text-2xl"
-            type="button"
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            ☰
-          </button>
-        </div>
-
-
-        
-      </div>
       </nav>
 
       {/* mobile navbar */}
@@ -111,25 +103,23 @@ const Header = () => {
               </Link>
             </li>
           ))}
-          
         </ul>
 
-          <Link
-            className="w-full text-white flex justify-center items-center mt-2 p-2 rounded-xl btn-glow"
-            href={""}
-          >
-            ثبت نام
-          </Link>
+        <Link
+          className="w-full text-white flex justify-center items-center mt-2 p-2 rounded-xl btn-glow"
+          href={""}
+        >
+          ثبت نام
+        </Link>
       </div>
 
-        {/* div for closing the navbar */}
-        <div
-          onClick={() => setIsOpen(!isOpen)}
-          className={`bg-transparent blur-3xl fixed w-full h-screen z-10 top-0 left-0 right-0 ${
-            isOpen ? "" : "hidden"
-          } `}
-        >
-        </div>
+      {/* div for closing the navbar */}
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className={`bg-transparent blur-3xl fixed w-full h-screen z-10 top-0 left-0 right-0 ${
+          isOpen ? "" : "hidden"
+        } `}
+      ></div>
     </>
   );
 };
